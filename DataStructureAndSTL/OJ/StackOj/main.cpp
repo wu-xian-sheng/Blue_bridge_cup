0af15c58e33081f7234aa1eb738001be30aab41e
bite-on-the-road <3513524983@qq.com>
2025-02-20 10:51:12 +0800

    StackOj题

---
+++ DataStructureAndSTL/OJ/StackOj/main.cpp
@@ -0,0 +1,83 @@
+//4.1 【模板】栈
+
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//typedef unsigned long long LL;
+//const int N = 1e6 + 10;
+//LL stk[N];
+//int top;
+//
+//
+//
+//int main()
+//{
+//	int T;
+//	cin >> T;
+//	while(T--)
+//	{
+//		//清空栈
+//		top = 0;
+//		
+//		int n;
+//		cin >> n;
+//		while(n--)
+//		{
+//			string op;
+//			cin >> op;
+//			if(op == "push")
+//			{
+//				LL x;
+//				cin >> x;
+//				stk[++top] = x;
+//			}
+//			else if(op == "pop")
+//			{
+//				if(top == 0)
+//					cout << "Empty" << endl;
+//				else
+//					top--;
+//			}
+//			else if(op == "query")
+//			{
+//				if(top == 0)
+//					cout << "Anguei!" << endl;
+//				else
+//					cout << stk[top] << endl;
+//			}
+//			else
+//			{
+//				cout << top << endl;
+//			}
+//		} 
+//		
+//	}
+//	return 0;
+//}
+
+
+
+
+
+//3.验证栈序列
+
+#include <iostream>
+#include <stack>
+using namespace std;
+const int N = 1e6 + 10;
+int n;
+int a[N], b[N];
+
+int main()
+{
+	
+	return 0;
+}
+
+
+
+
+
+
+
