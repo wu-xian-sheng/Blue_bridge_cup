fc43fa0bc501c49757451d10236f38602812d1f0
bite-on-the-road <3513524983@qq.com>
2025-03-01 00:18:53 +0800

    加油

--- DataStructureAndSTL/OJ/binaryTreeOJ/main.cpp
+++ DataStructureAndSTL/OJ/binaryTreeOJ/main.cpp
@@ -190,10 +190,92 @@
 
 
 #include <iostream>
+#include <vector>
+#include <queue>
 using namespace std;
+const int N = 1e5 + 10;
+int n;
+vector<int> edges[N];//存树 
 
+int fa[N], dist[N]; 
+
+//深度
+int dfs(int root)
+{
+	if(!root) return 0;
+	int ret = 0;
+	for(auto v: edges[root])
+	{
+		ret = max(ret, dfs(v));
+	}
+	return ret + 1;
+} 
+
+int bfs()
+{
+	queue<int> q;
+	q.push(1);
+	int ret = 0;
+	
+	while(q.size())
+	{
+		int sz = q.size();
+		ret = max(ret, sz);
+		
+		while(sz--)
+		{
+			int u = q.front();
+			q.pop();
+			
+			for(auto v: edges[u])
+			{
+				q.push(v);
+			}
+		}
+	}
+	
+	return ret;
+	
+}
  
- 
+int main()
+{
+	cin >> n;
+	for(int i =  1; i < n; i++)
+	{
+		//u->v;
+		int u, v; cin >> u >> v;
+		edges[u].push_back(v);
+		fa[v] = u;
+	 } 
+	 
+	 //深度 
+	 cout << dfs(1) << endl;
+	 
+	 //宽度
+	 cout << bfs() << endl;
+	 
+	 //求x到y的距离
+	 int x, y; cin >> x >> y;
+	 //x到根节点 
+	 while(x != 1)
+	 {
+	 	dist[fa[x]] = dist[x] + 1;
+	 	x = fa[x];
+	 }
+	 
+	 //y到指定位置
+	 int len = 0;
+	 while(y != 1 && dist[y] == 0) 
+	 {
+	 	len++;
+	 	y = fa[y];
+	 }
+	 
+	 cout << dist[y] * 2 + len << endl;
+	 	 
+	return 0;
+ } 
 
 
 
