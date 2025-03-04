9c0b4840f4600d618bc79114d174e34e9627259a
bite-on-the-road <3513524983@qq.com>
2025-03-04 19:01:10 +0800

    加油

--- DataStructureAndSTL/OJ/HeapOJ/main.cpp
+++ DataStructureAndSTL/OJ/HeapOJ/main.cpp
@@ -220,52 +220,151 @@
 
 //序列合并
 
+//#include <iostream>
+//#include <queue>
+//using namespace std;
+//
+//const int N = 1e5 + 10;
+//int a[N], b[N];
+//
+//int n;
+//
+//struct node
+//{
+//	int sum;
+//	int i, j;
+//	
+//	bool operator < (const node& x) const
+//	{
+//		//小根堆 
+//		return sum > x.sum;
+//	}	
+//
+//};
+//
+//priority_queue<node> heap; 
+//
+//int main()
+//{
+//	cin >> n;
+//	//放两个数列 
+//	for(int i = 1; i <= n; i++) cin >> a[i];
+//	for(int i = 1; i <= n; i++) cin >> b[i];
+//	
+//	
+//	//把a[i] + b[1] 放入堆 
+//	for(int i = 1; i <= n; i++)
+//	{
+//		heap.push({a[i] + b[1], i, 1});
+//	}
+//	
+//	while(n--)
+//	{
+//		auto t = heap.top(); heap.pop();
+//		int sum = t.sum, i = t.i, j = t.j;
+//		cout << sum << " ";
+//		heap.push({a[i] + b[j + 1] ,i ,j + 1});
+//	}
+//	
+//	return 0;
+//}
+
+
+//P1878 舞蹈课
+
 #include <iostream>
-#include <queue>
 using namespace std;
+#include <queue>
+#include <vector>
+#include <cmath>
 
-const int N = 1e5 + 10;
-int a[N], b[N];
+const int N = 2e5 + 10;
 
-int n;
+//双链表
+int e[N];
+int pre[N], ne[N];
 
 struct node
 {
-	int sum;
-	int i, j;
+	int a; //差值 
+	int l, r; //左右节点
 	
-	bool operator < (const node& x) const
+	bool operator<(const node &x) const
 	{
-		//小根堆 
-		return sum > x.sum;
+		if(a != x.a) return a > x.a;
+		else if(l != x.l) return l > x.l;
+		else return r > x.r;
 	}	
-
 };
 
-priority_queue<node> heap; 
+int n;
+int s[N];
+bool st[N]; 
+
+priority_queue<node> heap;
 
 int main()
 {
 	cin >> n;
-	//放两个数列 
-	for(int i = 1; i <= n; i++) cin >> a[i];
-	for(int i = 1; i <= n; i++) cin >> b[i];
-	
-	
-	//把a[i] + b[1] 放入堆 
 	for(int i = 1; i <= n; i++)
 	{
-		heap.push({a[i] + b[1], i, 1});
+		char c;
+		cin >> c;
+		if(c == 'B') s[i] = 1; //标记性别 
 	}
 	
-	while(n--)
+	for(int i = 1; i <= n; i++)
+	{
+		//读入双链表 
+		cin >> e[i];
+		pre[i] = i - 1;
+		ne[i] = i + 1;
+	}
+	pre[1] = ne[n] = 0;//将链表中的最后一个节点的ne值改为0；
+	
+	//插入堆
+	for(int i = 2; i <= n; i++) 
+	{
+		if(s[i] != s[i - 1])
+		{
+			heap.push({abs(e[i] - e[i - 1]), i - 1, i});
+		}
+		
+	}
+	
+	vector<node> ret;
+	
+	while(heap.size())
 	{
 		auto t = heap.top(); heap.pop();
-		int sum = t.sum, i = t.i, j = t.j;
-		cout << sum << " ";
-		heap.push({a[i] + b[j + 1] ,i ,j + 1});
+		int a = t.a, l = t.l, r = t.r;
+		
+		if(st[l] || st[r]) continue;
+		
+		ret.push_back(t);
+		
+		st[l] = st[r] = true;
+		
+		//修改指针，还原链表 
+		ne[pre[l]] = ne[r];
+		pre[ne[r]] = pre[l];
+		
+		
+		int left = pre[l], right = ne[r];
+		if(left && right && s[left] != s[right])
+		{
+			heap.push({abs(e[left] - e[right]), left, right});
+		}
+		
+		
 	}
 	
+	cout << ret.size() << endl;
+	
+	for(auto& x: ret)
+	{
+		cout << x.l << " " << x.r << endl;
+	}
 	return 0;
 }
 
