a8eaef720bc04bba97231fc0efcd0742b48a64fd
bite-on-the-road <3513524983@qq.com>
2025-03-10 00:39:14 +0800

    加油

--- algorithmFoundation/02gaojindu/main.cpp
+++ algorithmFoundation/02gaojindu/main.cpp
@@ -112,59 +112,98 @@
 //高精度乘法
 
 
+//#include <iostream>
+//using namespace std;
+//
+//const int N = 1e6 + 10;
+//
+//int a[N], b[N], c[N];
+//int la, lb, lc;
+//
+//void mul(int a[], int b[], int c[])
+//{
+//	for(int i = 0; i < la; i++)
+//	{
+//		for(int j = 0; j < lb; j++)
+//		{
+//			c[i + j] += a[i] * b[j];
+//		}
+//	}
+//	
+//	//处理进位
+//	for(int i = 0; i < lc; i++)
+//	{
+//		c[i + 1] += c[i] / 10;
+//		c[i] %= 10;
+//	}
+//	 
+//	while(lc > 1 && c[lc - 1] == 0) lc--;
+//	
+//}
+//
+//int main()
+//{
+//	string x, y;
+//	cin >> x >> y;
+//	
+//	//拆分 放在逆序放在数组中
+//	la = x.size(); lb = y.size(); lc = la + lb; 
+//	for(int i = 0; i < la; i++) a[la - 1 - i] = x[i] - '0';
+//	for(int i = 0; i < lb; i++) b[lb - 1 - i] = y[i] - '0';
+//	
+//	//模拟乘法过程
+//	mul(a, b, c);
+//	
+//	//输出
+//	for(int i = lc - 1; i >= 0; i--) cout << c[i];
+//	
+//	return 0;
+//}
+
+
+
+
+
+//高精度除法
+
 #include <iostream>
 using namespace std;
+typedef long long LL;
 
 const int N = 1e6 + 10;
+int a[N], c[N];
+int b;
+int la, lc;
 
-int a[N], b[N], c[N];
-int la, lb, lc;
-
-void mul(int a[], int b[], int c[])
+void sub(int a[], int b, int c[])
 {
-	for(int i = 0; i < la; i++)
+	LL t = 0; //统计余数 
+	for(int i = la - 1; i >= 0; i--)
 	{
-		for(int j = 0; j < lb; j++)
-		{
-			c[i + j] += a[i] * b[j];
-		}
+		t = t * 10 + a[i];
+		c[i] = t / b;
+		t %= b;
 	}
 	
-	//处理进位
-	for(int i = 0; i < lc; i++)
-	{
-		c[i + 1] += c[i] / 10;
-		c[i] %= 10;
-	}
-	 
-	while(lc > 1 && c[lc - 1] == 0) lc--;
-	
+	//处理前导0
+	while(lc > 1 && c[lc - 1] == 0) lc--; 
 }
 
 int main()
 {
-	string x, y;
-	cin >> x >> y;
-	
-	//拆分 放在逆序放在数组中
-	la = x.size(); lb = y.size(); lc = la + lb; 
+	string x;
+	cin >> x >> b;
+	la = x.size();
+	lc = la;
 	for(int i = 0; i < la; i++) a[la - 1 - i] = x[i] - '0';
-	for(int i = 0; i < lb; i++) b[lb - 1 - i] = y[i] - '0';
 	
-	//模拟乘法过程
-	mul(a, b, c);
+	sub(a, b, c);
 	
-	//输出
+	
 	for(int i = lc - 1; i >= 0; i--) cout << c[i];
 	
 	return 0;
-}
-
-
-
-
-
-
+} 
 
 
 
